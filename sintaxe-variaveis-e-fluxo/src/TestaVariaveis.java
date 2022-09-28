3d357d745f2a2a07b02cb7274262407b83a0b74a
lucas arcanjo <lucas.saolliveira@gmail.com>
2022-09-28 15:14:20 -0300

    fisrt commit

---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,20 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("ola novo teste");
+		
+		int idade;
+		idade = 25;
+		
+		System.out.println(idade);
+		
+		idade = 4 * (5 + 5);
+		System.out.print(idade);
+		idade = 4 * 5 + 5;
+		
+		System.out.println(idade);
+		
+		System.out.println("a idade é " + idade + ", parabens");
+	}
+}
