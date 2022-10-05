516711b38ce8f3ab2938e9746124a921eeebb9e7
lucas arcanjo <lucas.saolliveira@gmail.com>
2022-10-05 10:37:17 -0300

    attribute and encapsulation private

--- byteback-composto/src/TestaBancoSemCliente.java
+++ byteback-composto/src/TestaBancoSemCliente.java
@@ -5,7 +5,7 @@ public class TestaBancoSemCliente {
 		
 		Conta contaLucas = new Conta();
 		
-		System.out.println(contaLucas.saldo);
+		System.out.println(contaLucas.pegaSaldo());
 		System.out.println(contaLucas.titular);
 		contaLucas.titular = new Cliente();
 		contaLucas.titular.nome = "Lucas Arcanjo";
