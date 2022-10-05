f401247aa3c6b2eb1471098d3873db357211d3ff
lucas arcanjo <lucas.saolliveira@gmail.com>
2022-10-05 16:37:43 -0300

    getters and setters of reference

--- byteback-encapsulamento/src/Conta.java
+++ byteback-encapsulamento/src/Conta.java
@@ -43,4 +43,11 @@ public class Conta {
 	public void setAgencia(int agencia) {
 		this.agencia = agencia;
 	}
+	public Cliente getTitular() {
+		return titular;
+	}
+	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
 }
--- byteback-encapsulamento/src/TestaGetSet.java
+++ byteback-encapsulamento/src/TestaGetSet.java
@@ -6,5 +6,18 @@ public class TestaGetSet {
 		Conta conta = new Conta();
 		conta.setNumero(1227);
 		System.out.println(conta.getNumero());
+		
+		Cliente lucas = new Cliente();
+		
+		lucas.setNome("lucas");
+		System.out.println(lucas.getNome());
+		
+		Conta contaTitular = new Conta();
+		contaTitular.setTitular(lucas);
+		
+		System.out.println(contaTitular.getTitular().getNome());
+		
+		contaTitular.getTitular().setProfissao("developer");
+		System.out.println(contaTitular.getTitular().getProfissao());
 	}
 }
