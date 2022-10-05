cf841242672e24bd197853de8cf682fa9837992c
lucas arcanjo <lucas.saolliveira@gmail.com>
2022-10-05 11:52:36 -0300

    getters and setters

---
+++ byteback-encapsulamento/src/TestaGetSet.java
@@ -0,0 +1,10 @@
+
+public class TestaGetSet {
+
+	public static void main(String[] args) {
+		
+		Conta conta = new Conta();
+		conta.setNumero(1227);
+		System.out.println(conta.getNumero());
+	}
+}
