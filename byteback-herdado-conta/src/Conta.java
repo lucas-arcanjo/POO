861049fd06bbf3798d5c3ce87c0c8c8ebdd23665
lucas arcanjo <lucas.saolliveira@gmail.com>
2022-10-10 13:51:15 -0300

    inheritance and constructors

---
+++ byteback-herdado-conta/src/Conta.java
@@ -0,0 +1,73 @@
+public class Conta {
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private static int total;
+	
+	public Conta(int agencia, int numero) {
+		total++;
+		System.out.println("estou criando uma conta, total de contas " + total);
+		this.agencia = agencia;
+		this.numero = numero;
+	}
+	
+	void deposita(double valor) {
+		this.saldo += valor;
+	}
+	
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean tranfere(double valor, Conta destino) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.saldo += valor;
+			return true;
+		}
+		return false;
+	}
+	
+	public double getSaldo() {
+		return this.saldo;
+	}
+
+	public double getNumero() {
+		return this.numero;
+	}
+	 
+	public void setNumero(int numero) {
+		if (numero <= 0) {
+			System.out.println("o número não pode ser menor ou igual a zero");
+			return;
+		}
+		this.numero = numero;
+	}
+	public int getAgencia() {
+		return this.agencia;
+	}
+	public void setAgencia(int agencia) {
+		if (agencia <= 0) {
+			System.out.println("a agencia não pode ser menor ou igual a zero");
+			return;
+		}
+		this.agencia = agencia;
+	}
+	public Cliente getTitular() {
+		return titular;
+	}
+	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+	public static int getTotal() {
+		return total;
+	}
+}
---
+++ byteback-herdado-conta/src/ContaPoupanca.java
@@ -0,0 +1,7 @@
+
+public class ContaPoupanca extends Conta {
+
+	ContaPoupanca(int agencia, int numero) {
+		super(agencia, numero);
+	}
+}
